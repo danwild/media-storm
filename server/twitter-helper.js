c333a65723a43197ea4d5a9f7e9b1cf7f40dc789
daniel-k-richardson <danielr9@utas.edu.au>
2017-07-29 14:36:50 +1000

    small fixes to twitter api

--- server/twitter-helper.js
+++ server/twitter-helper.js
@@ -4,50 +4,31 @@
 
 TwitterHelper = {
 
+    accessToken : "icNZWaoOvwjGIknimKODwwn4j",
+    accessTokenSecret : "ItbA7XFZJO9mNTuRVWGOF1iMbUgw2HAWhQNClv1MLcaNYxGp3c",
 
+    oauth : new OAuth.OAuth(
+        'https://api.twitter.com/oauth/request_token',
+        'https://api.twitter.com/oauth/access_token',
+        this.accessToken,
+        this.accessTokenSecret,
+        '1.0A',
+        null,
+        'HMAC-SHA1'
+    ),
 
-    constructor () {
-
-
-        var accessToken = "icNZWaoOvwjGIknimKODwwn4j";
-        var accessTokenSecret = "ItbA7XFZJO9mNTuRVWGOF1iMbUgw2HAWhQNClv1MLcaNYxGp3c";
-
-
-        var OAuth= require('oauth').OAuth;
-
-        // var oa = new OAuth(
-        //     "https://api.twitter.com/oauth/request_token",
-        //     "https://api.twitter.com/oauth/access_token",
-        //     "icNZWaoOvwjGIknimKODwwn4j",
-        //     "ItbA7XFZJO9mNTuRVWGOF1iMbUgw2HAWhQNClv1MLcaNYxGp3c",
-        //     "1.0",
-        //     "http://media-storm.tech",
-        //     "HMAC-SHA1"
-        // );
-
-
-        var oauth = new OAuth.OAuth(
-            'https://api.twitter.com/oauth/request_token',
-            'https://api.twitter.com/oauth/access_token',
-            accessToken,
-            accessTokenSecret,
-            '1.0A',
-            null,
-            'HMAC-SHA1'
-        );
+    getRequestId: function () {
 
         //GET /search/tweets.json
-        oauth.get(
+        this.oauth.get(
             'https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi',
-            accessToken,
-            accessTokenSecret,
-            function (e, data, res){
+            this.accessToken,
+            this.accessTokenSecret,
+            function (e, data, res) {
                 if (e) console.error(e);
                 console.log(data);
             });
-    }
 
-
-
+    },
 
 };
